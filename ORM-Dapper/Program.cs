ff52a6860ca8040d657b1146bbdd777406b65049
Austin Okyere <okyereaustin@gmail.com>
2026-03-17 21:41:42 -0600

    Completed ORM exercise 1

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace ORM_Dapper
-{
-    public class Program
+﻿using System.Data;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using ORM_Dapper;
+
+
+var config = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+    string connString = config.GetConnectionString("DefaultConnection");
+
+    IDbConnection conn = new MySqlConnection(connString);
+
+    var departmentRepo = new DapperDepartmentRepository(conn);
+
+    var departments = departmentRepo.GetAllDepartments();
+
+    foreach (var d in departments)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine(d.DepartmentID);
+        Console.WriteLine(d.Name);
     }
-}
+
