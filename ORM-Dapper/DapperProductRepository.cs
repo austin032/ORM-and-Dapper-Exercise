4046a4d411999b4aa3d12844011846c9f4719bd3
Austin Okyere <okyereaustin@gmail.com>
2026-03-18 19:15:04 -0600

    Completed orm exercise 1 & 2

---
+++ ORM-Dapper/DapperProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using Dapper;
+
+namespace ORM_Dapper;
+
+public class DapperProductRepository : IProductRepository
+{
+    private readonly IDbConnection _conn;
+
+    public DapperProductRepository(IDbConnection conn)
+    {
+        _conn = conn;
+    }
+
+    public IEnumerable<Product> GetAllProducts()
+    {
+        return _conn.Query<Product>("SELECT * FROM Products");
+    }
+
+    
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -13,13 +13,27 @@ var config = new ConfigurationBuilder()
 
     IDbConnection conn = new MySqlConnection(connString);
 
-    var departmentRepo = new DapperDepartmentRepository(conn);
+    //var departmentRepo = new DapperDepartmentRepository(conn);
 
-    var departments = departmentRepo.GetAllDepartments();
+    //var departments = departmentRepo.GetAllDepartments();
 
-    foreach (var d in departments)
+    //foreach (var d in departments)
     {
-        Console.WriteLine(d.DepartmentID);
-        Console.WriteLine(d.Name);
+        //Console.WriteLine(d.DepartmentID);
+        //Console.WriteLine(d.Name);
     }
+
+      var productsRepo = new DapperProductRepository(conn);
+      
+      var products = productsRepo.GetAllProducts();
+
+      foreach (var p in products)
+      {
+          Console.WriteLine(p.ProductID);
+          Console.WriteLine(p.Name);
+          Console.WriteLine(p.Price);
+          Console.WriteLine(p.CategoryID);
+          Console.WriteLine(p.OnSale);
+          Console.WriteLine(p.StockLevel);
+      }
 
